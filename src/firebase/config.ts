41e26600d01fb2e77fb0fbcc94950fe31bf596b9
Wataru Ono <wata-dev@WatarunoMacBook-Pro.local>
2020-12-30 16:59:50 +0900

    :sparkles: firebaseの設定

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZ3Dols_iyjE-0ZxKCcBBzrrbHhcOm3nI",
+  authDomain: "gift-change-c6085.firebaseapp.com",
+  projectId: "gift-change-c6085",
+  storageBucket: "gift-change-c6085.appspot.com",
+  messagingSenderId: "143736147452",
+  appId: "1:143736147452:web:cef9c67186be42740eb679",
+  measurementId: "G-CRPVPG7TTG"
+}
