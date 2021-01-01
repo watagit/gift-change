01dfbaf464db617c94d1ed32db4fbb6996d2bb68
Wataru Ono <wata-dev@WatarunoMacBook-Pro.local>
2021-01-01 21:23:15 +0900

    :art: Containerで囲った

--- src/components/templates/Top.tsx
+++ src/components/templates/Top.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Container } from 'semantic-ui-react'
 
 import GlobalMenu from '../organisms/GlobalMenu'
 
 const Top: React.FC = () => {
-  return <GlobalMenu />
+  return (
+    <Container>
+      <GlobalMenu />
+    </Container>
+  )
 }
 
 export default Top
