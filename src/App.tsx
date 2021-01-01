27173d3fae135945fdd03382079970e56efbd294
Wataru Ono <wata-dev@WatarunoMacBook-Pro.local>
2021-01-01 21:28:20 +0900

    :sparkles: ルーティングの設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Top from './components/templates/Top'
+import Home from './components/templates/Home'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path={'/'} component={Top} />
+        <Route exact path={'/home'} component={Home} />
       </Switch>
     </BrowserRouter>
   )
